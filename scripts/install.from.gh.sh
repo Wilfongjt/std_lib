d183ce4f5c28f351fdeedb1fcab9a0f8f455d087
Wilfongjt <wilfongjt@gmail.com>
2023-06-22 10:22:30 -0400

    adjust

--- scripts/install.from.gh.sh
+++ scripts/install.from.gh.sh
@@ -2,7 +2,20 @@
 cd ..
 ## Syntax: pip install "Package" @ git+"URL of the repository"
 ## eg pip install pip@git+https://github.com/pypa/pip
-pip install std_lib@git+https://github.com/Wilfongjt/std_lib
+pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std-lib-0.1.0
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib-0.0.0
+
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std-lib-0.1.0
+
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib-0.1.0
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib
+
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib_package.0.1.0
+
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib_package.0.1.0
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib_package
+
+#pip install std_lib@git+https://github.com/Wilfongjt/std_lib#egg=std_lib
 
 #pip install git+https://github.com/Wilfongjt/std_lib.git#egg=my_pip_package
-#pip install git+git://github.com/Wilfongjt/std_lib_package.git#egg=my_pip_package
+#pip install git+git://github.com/Wilfongjt/std_lib.git#egg=my_pip_package
