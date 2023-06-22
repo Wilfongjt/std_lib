b577d66e4a480046a5f7cc5736c52f081e47aaca
Wilfongjt <wilfongjt@gmail.com>
2023-06-22 08:34:37 -0400

    change-hello_world.py-to-greetings.py

--- scripts/confirm.package.import.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ..
-python3 -c "import std_lib_package; std_lib_package.hello_world()"
--- std_lib_package/__init__.py
+++ std_lib_package/__init__.py
@@ -1,1 +1,6 @@
-__version__ = 'dev'
+__version__ = '0.1.0'
+
+__all__ = [
+    'greetings',
+    'math'
+]
