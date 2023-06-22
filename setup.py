eae07d47b8b1905ff984f4eca80123623967fa11
Wilfongjt <wilfongjt@gmail.com>
2023-06-22 09:03:57 -0400

    fix-package-name

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from std_lib_package import __version__
 setup(
     name='std_lib_package',
     version=__version__,
-
     url='https://github.com/Wilfongjt/std_lib',
     author='James Wilfong',
     author_email='wilfongjt@gmail.com',
